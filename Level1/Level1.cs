76f34f18b5c9aa18a46df358e3df11c29fef2e4a
Ranes <ranesatsea@users.noreply.github.com>
2020-10-05 17:15:26 -0400

    Spike can properly move left and right -Added signal to Left and Right Node whiwch allows spike to properly move left and right

--- Level1/Level1.cs
+++ Level1/Level1.cs
@@ -4,41 +4,41 @@ using System;
 public class Level1 : Node2D
 {
     private static int spikeCheck;
-    public static int buttonOneCheck=0;
+    // public static int buttonOneCheck=0;
     public int speed=100;
 
     public void on_WireMappingLevel1_body_entered(KinematicBody2D body2D)
     {
         GD.Print("sds");
     }
-    public override void _Process(float delta)
-    {
-        float Amount =1;
-        var velocity=new Vector2(Amount,0);
-        //var movingSpike = (spike)GetNode("../root/spike");
-
-        if(buttonOneCheck == 1)// && leftRight == 0
-        {
-            KinematicBody2D movingSpike = this.GetNode<KinematicBody2D>("Spike");
-            movingSpike.Position += velocity *delta * speed;
-        }
-        else if(spikeCheck==0)
-        {
-            var speed=0;
-        }
-    }
+    // public override void _Process(float delta)
+    // {
+    //     float Amount =1;
+    //     var velocity=new Vector2(Amount,0);
+    //     //var movingSpike = (spike)GetNode("../root/spike");
+        
+    //     if(Trigger.buttonOneCheck == 1)// && leftRight == 0
+    //     {
+    //         KinematicBody2D movingSpike = this.GetNode<KinematicBody2D>("Spike");
+    //         movingSpike.Position += velocity *delta * speed;
+    //     }
+    //     else if(spikeCheck==0)
+    //     {
+    //         var speed=0;
+    //     }
+    // }
     //Signal for Trigger
-    public void _on_Trigger_body_entered(KinematicBody2D body)
-    {
-        GD.Print("Enter trigger");
-        buttonOneCheck= 1;
-    }
+    // public void _on_Trigger_body_entered(KinematicBody2D body)
+    // {
+    //     GD.Print("Enter trigger");
+    //     buttonOneCheck= 1;
+    // }
 
-    public void _on_Trigger_body_exited(KinematicBody2D body)
-    {
-        buttonOneCheck= 0;
-        GD.Print("Exit trigger");
-    }
+    // public void _on_Trigger_body_exited(KinematicBody2D body)
+    // {
+    //     buttonOneCheck= 0;
+    //     GD.Print("Exit trigger");
+    // }
    
 
 
--- Trigger/Trigger.cs
+++ Trigger/Trigger.cs
@@ -3,14 +3,64 @@ using System;
 
 public class Trigger : Area2D
 {
-    public override void _Ready()
-    {
-        
-    }
-    public void on_Left_body_entered(KinematicBody2D body)
-    {
+    public static int buttonOneCheck=0;
+    public static int leftRight=0;
+    public int speed=100;
     
-
+    /*
+    When Player steps on Trigger Node to see if spikes will be moving
+    */
+    public void _on_Trigger_body_entered(KinematicBody2D body)//Player steps on nodes, the spike willmove
+    {
+        GD.Print("Enter trigger");
+        buttonOneCheck= 1;
     }
 
+    public void _on_Trigger_body_exited(KinematicBody2D body)//when playe steps out, spike will stop
+    {
+        buttonOneCheck= 0;
+        GD.Print("Exit trigger");
+    }
+    
+    /*
+    Check to see if the spike will move left or right
+    */
+    public override void _Process(float delta)
+    {
+        float Amount =1;
+        var velocity=new Vector2(Amount,0);
+        
+        if(buttonOneCheck == 1 && leftRight == 0)
+        {
+            KinematicBody2D movingSpike = this.GetNode<KinematicBody2D>("Spike");
+            movingSpike.Position += velocity *delta * speed;
+        }
+        else if(buttonOneCheck == 1 && leftRight == 1)
+        {
+            KinematicBody2D movingSpike = this.GetNode<KinematicBody2D>("Spike");
+            movingSpike.Position -= velocity *delta * speed; 
+        }
+    }
+    /*
+    Signal
+    
+    */
+    public void on_Right_body_entered(KinematicBody2D body)
+    {
+        if(body.GetName() == "Spike")
+        {
+            leftRight = 1;
+            GD.Print("SPike will now move RIght");
+        }
+    }
+    public void on_Left_body_entered(KinematicBody2D body2D)
+    {
+        if(body2D.GetName()== "Spike")
+        {
+            leftRight=0;
+            GD.Print("Spike will now move Left");
+        }
+    }
+
+   
 }
